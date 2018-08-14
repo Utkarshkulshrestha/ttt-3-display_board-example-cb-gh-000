9d3a4d00c71fa1e467d183ecdcbad6a33ba63852
utkarsh kulshrestha <15030121028@sicsr.ac.in>
2018-08-14 18:42:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+spaces = 0
+dash = 0
+def display_board
+  begin
+    begin
+      puts("   ")
+      puts("|")
+      spaces++
+   end when spaces <= 3
+   puts(----------)
+   dash ++
+ end when dash < 4
+   
